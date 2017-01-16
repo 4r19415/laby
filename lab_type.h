e915cf6329348c13002cc4d5e28abf3ea615d4a8
BEZIEAU Justin <3674148@pc4021.polytech.upmc.fr>
2017-01-16 23:18:55 +0100

    comment 4

--- lab_type.h
+++ lab_type.h
@@ -30,38 +30,52 @@
 #define lab_type_h
 
 typedef struct {
+/** 
+	structure of all the labyrinth
+	contain data of : the lab
+			  the players
+			  the treasure
+*/
 
-	char ** plateau;
-	int sizeX;
-	int sizeY;
+	char ** plateau; // the matrix of the lab, contain ' ' ou '|'
+	int sizeX; // the number of columns
+	int sizeY; // the number of lines
 	
-	int j1_posX;
-	int j1_posY;
-	int j1_energy;
+	int j1_posX; // x of player1
+	int j1_posY; // y of player1
+	int j1_energy; // energy of player1
 	
-	int j2_posX;
-	int j2_posY;
-	int j2_energy;
+	int j2_posX; // X of player2
+	int j2_posY; // y of player2
+	int j2_energy; // energy of player2
 	
-	int t_posX;
-	int t_posY;
+	int t_posX; // x of the treasure
+	int t_posY; // y of the treasure
 	
-	t_move last_move;
+	t_move last_move; // the last move done in the lab, not used ^^
 	
-	int lab_has_change;
+	int lab_has_change; // if there is a rotation noot viewed 
 
 }Lab;
 
 typedef struct prev{
+/** 
+	structure of a case of the lab
+	contain data of : the position
+			  the distance between this case and the treasure
+			  the case before this case in the A* (cf a_star.c)
+			  if the case contain the treasure
+*/
 
-	int x,y;
-	int cost;
-	int heuristique;
-	int xp, yp;
+	int x,y; // the x and y of this case
+	int cost; // the distance between this case 
+		  // and the player who launch the A*
+	int heuristique; // the distance between this case and the tresure
+	int xp, yp; // coordonate of the previous case
 	
-	struct prev * p_case;
+	struct prev * p_case; // pointer on the previous case
 	
-	int treasure;
+	int treasure; // thi case contain the treasure ? if yes = 1
 
 
 }Case;
